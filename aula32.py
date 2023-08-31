2b11c77ce9666a3ad0d84b87ea398a316c9c1708
newberg85 <bergviana12345@gmail.com>
2023-08-31 20:34:04 -0300

    Cd3

--- aula32.py
+++ aula32.py
@@ -2,5 +2,8 @@
 https://docs.python.org/pt-br/3/library/stdtypes.html
 Imutáveis que vimos: str, int, float, bool
 """
-
-string = 'New Berg'
+string = '1000'
+# outra_variavel = f'{string[:3]}ABC{string[4:]}'
+# print(string)
+# print(outra_variavel)
+print(string.zfill(10))

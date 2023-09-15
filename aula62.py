50cfde02f7995fc556219f770de166f519e7a0c2
newberg85 <bergviana12345@gmail.com>
2023-09-15 20:03:03 -0300

    Cd10

---
+++ aula62.py
@@ -0,0 +1,15 @@
+"""
+Operação ternária (condicional de uma linha)
+<valor> if <condicao> else <outro valor>
+"""
+
+# condicao = 10 == 10
+# variavel = 'Valor' if condicao else 'Outro valor'
+# print()
+
+# digito = 9
+# # novo_digito = digito if digito <= 9 else 0 
+# novo_digito = 0 if digito > 9 else digito
+# print(novo_digito)
+
+print('Valor' if True else 'Outro valor' if True else 'Fim')
---
+++ aula64-exercicio.py
@@ -0,0 +1,31 @@
+import random
+import sys
+
+# primeiro digito do cpf
+nove_digitos = ''
+for i in range(9):
+    nove_digitos += str(random.randint(0, 9))
+
+contador_regressivo_1 = 10 
+
+resultado_digito_1 = 0 
+for digito in nove_digitos:
+    resultado_digito_1 += int(digito) * contador_regressivo_1
+    contador_regressivo_1 -= 1
+digito_1 = (resultado_digito_1 * 10) % 11
+digito_1 = digito_1 if digito_1 <= 9 else 0
+
+# segundo digito do cpf
+dez_digitos = nove_digitos + str(digito_1)
+
+contador_regressivo_2 = 11
+resultado_digito_2 = 0
+
+for digito in dez_digitos:
+    resultado_digito_2 += int(digito) * contador_regressivo_2
+    contador_regressivo_2 -= 1
+digito_2 = (resultado_digito_2 * 10) % 11
+digito_2 = digito_2 if digito_2 <= 9 else 0
+
+cpf_gerado = f'{nove_digitos}{digito_1}{digito_2}'
+print(cpf_gerado)
